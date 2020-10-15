d8902eb8fd5218a0340a6ab1c45f38fcb9d2498d
Luka Jajanidze <40858562+LukaJaj@users.noreply.github.com>
2020-10-15 16:19:59 +0400

    Update main.java

--- main.java
+++ main.java
@@ -4,12 +4,6 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Task_1 task1 = new Task_1();
-		MyStack stack = new MyStack();
-		
-		
-		
-		
 		System.out.println("Hello from eclipse once more");
 		
 
